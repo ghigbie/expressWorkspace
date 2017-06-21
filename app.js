82d02d074e2b7bdaf426bc7f1faf337ca969c1a9
George Higbie <gehigbie@gmail.com>
2017-06-21 17:05:46 +0000

    Add new routes and res.sendFile() method

--- app.js
+++ app.js
@@ -1,21 +1,28 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
-app.set('port', 3000);
+app.set('port', process.env.PORT);
 app.set('ip', process.env.IP);
 
 app.get('/', (req, res) =>{
     console.log(`GET the homepage`);
     res
-    .status(200)
-    .send("Homepage");
+     .status(200)
+     .sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.get('/json', (req, res) => {
-   console.log("GET the json");
+   console.log('GET the json');
    res
     .status(200)
     .json( {"jsonData" : true} );
+});
+
+app.get('/file', (req, res) => {
+   res
+    .status(200)
+    .sendFile(path.join(__dirname, 'app.js'));
 });
 
 var server = app.listen(app.get('port'), app.get('ip'), () => {
