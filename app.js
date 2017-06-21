614ef0197298eda0f2dc7ad91e2c963cedfa1619
George Higbie <gehigbie@gmail.com>
2017-06-21 16:05:09 +0000

    Complete listen process

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 
-const port = 3000;
-const ip = process.env.IP;
-app.listen(port, ip, () => {
+app.set('port', 3000);
+app.set('ip', process.env.IP);
+
+var server = app.listen(app.get('port'), app.get('ip'), () => {
+    var port = server.address().port;
     console.log(`Express server listening on port ${port}.`);
 });
