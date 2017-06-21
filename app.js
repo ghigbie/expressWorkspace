71c9777e8a8c0e43efc4b74e6a5e0c2fdbfab38e
George Higbie <gehigbie@gmail.com>
2017-06-21 15:54:40 +0000

    Set express server to listen

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log("yo");
+const express = require('express');
+const app = express();
+
+const port = 3000;
+const ip = process.env.IP;
+app.listen(port, ip, () => {
+    console.log(`Express server listening on port ${port}.`);
+});
