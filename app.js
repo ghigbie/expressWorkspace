7df22507f278bc6593a29b522ed1b1f205a3a7f1
George Higbie <gehigbie@gmail.com>
2017-06-21 16:11:22 +0000

    Add two routes

--- app.js
+++ app.js
@@ -4,6 +4,20 @@ const app = express();
 app.set('port', 3000);
 app.set('ip', process.env.IP);
 
+app.get('/', (req, res) =>{
+    console.log(`GET the homepage`);
+    res
+    .status(200)
+    .send("Homepage");
+});
+
+app.get('/json', (req, res) => {
+   console.log("GET the json");
+   res
+    .status(200)
+    .json( {"jsonData" : true} );
+});
+
 var server = app.listen(app.get('port'), app.get('ip'), () => {
     var port = server.address().port;
     console.log(`Express server listening on port ${port}.`);
