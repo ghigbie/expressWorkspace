a59800655a09a64a9898799344f6293ed1668c49
George Higbie <gehigbie@gmail.com>
2017-06-22 16:55:40 +0000

    Add minified css file

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 app.set('port', process.env.PORT);
 app.set('ip', process.env.IP);
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/public', express.static(path.join(__dirname, 'public')));
 
 // app.get('/', (req, res) =>{
 //     console.log(`GET the homepage`);
