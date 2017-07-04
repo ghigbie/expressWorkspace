f04153c1be77aef6a52987abf287d29d4c75184d
George Higbie <gehigbie@gmail.com>
2017-07-04 21:35:13 +0000

    Add new routes

--- app-two.js
+++ app-two.js
@@ -1,11 +1,25 @@
 const express = require("express");
 const app = express();
 
-app.set('port', 3000);
+app.set('port', process.env.PORT);
 app.set('ip', process.env.IP);
 
-app.listen(app.get('port'), app.get('ip'), () => {
-    var port = app.get('port');
+app.get('/', (req, res) => {
+    console.log(`GET the homepage`);
+    res
+        .status(200)
+        .send("<h1>Home page!!!</h1>");
+});
+
+app.get('/json', (req, res) => {
+    console.log(`GET the json`);
+    res
+        .status(200)
+        .json({"jsonData": true});
+});
+
+var server = app.listen(app.get('port'), app.get('ip'), () => {
+    var port = server.address().port;
     var ip = app.get('ip');
     console.log(`Server is listening on port ${port}`);
     console.log(`Ip is set to ${ip}`);
